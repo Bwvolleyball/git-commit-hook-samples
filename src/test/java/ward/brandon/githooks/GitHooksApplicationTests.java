67ca672ed7be2456d163df2311c48c0e84c0f66b
Brandon Ward <ward486@regis.edu>
2017-08-22 23:23:27 -0600

    documented provided test and included (dummy) sample assertion.

--- src/test/java/ward/brandon/githooks/GitHooksApplicationTests.java
+++ src/test/java/ward/brandon/githooks/GitHooksApplicationTests.java
@@ -1,5 +1,8 @@
 package ward.brandon.githooks;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GitHooksApplicationTests {
 
+	/**
+	 * Empty test provided by Spring Boot to ensure that the application starts.
+	 */
 	@Test
 	public void contextLoads() {
+		// sample (dummy) assertion
+		assertThat(2, is(2));
 	}
 
 }
