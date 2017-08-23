d502526ef9eeb123613c02d62ea219d90e216448
Brandon Ward <ward486@regis.edu>
2017-08-22 23:13:02 -0600

    converting sample project to include a small demo spring boot app

---
+++ src/main/java/ward/brandon/githooks/GitHooksApplication.java
@@ -0,0 +1,12 @@
+package ward.brandon.githooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHooksApplication.class, args);
+	}
+}
---
+++ src/test/java/ward/brandon/githooks/GitHooksApplicationTests.java
@@ -0,0 +1,16 @@
+package ward.brandon.githooks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitHooksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
